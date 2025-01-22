c8a0fb54c24655b15cc9398187745f49a27adbab
Ayodeji Dennis <iamluwaden@gmail.com>
2025-01-23 00:10:26 +0100

    responsive

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(0px)"
-}
-function cancel(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(-500px)"
-}
-// Typewriter Effect
-const texts = [
-    "SOFTWARE DEVELOPER",
-    "RESEARCHER",
-    "PRODUCT MANAGER",
-    "COMMUNITY MANAGER",
-  
-]
-let speed  =100;
-const textElements = document.querySelector(".typewriter-text");
-let textIndex = 0;
-let charcterIndex = 0;
-function typeWriter(){
-    if (charcterIndex < texts[textIndex].length){
-        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
-        charcterIndex++;
-        setTimeout(typeWriter, speed);
+document.addEventListener("DOMContentLoaded", function() {
+    // Dropdown functionality
+    const navbar = document.querySelector(".dropdown");
+    const toggleButton = document.querySelector(".navbar-toggler");
+
+    toggleButton.addEventListener("click", function() {
+        if (navbar.style.transform === "translateY(0px)") {
+            navbar.style.transform = "translateY(-500px)";
+        } else {
+            navbar.style.transform = "translateY(0px)";
+        }
+    });
+
+    // Typewriter Effect
+    const texts = [
+        " A SOFTWARE DEVELOPER",
+        " A RESEARCHER",
+        " A PRODUCT MANAGER",
+        " A COMMUNITY MANAGER",
+    ];
+    let speed = 100;
+    const textElements = document.querySelector(".typewriter-text");
+    let textIndex = 0;
+    let characterIndex = 0;
+
+    function typeWriter() {
+        if (characterIndex < texts[textIndex].length) {
+            textElements.innerHTML += texts[textIndex].charAt(characterIndex);
+            characterIndex++;
+            setTimeout(typeWriter, speed);
+        } else {
+            setTimeout(eraseText, 1000);
+        }
     }
-    else{
-        setTimeout(eraseText, 1000)
+
+    function eraseText() {
+        if (textElements.innerHTML.length > 0) {
+            textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+            setTimeout(eraseText, 50);
+        } else {
+            textIndex = (textIndex + 1) % texts.length;
+            characterIndex = 0;
+            setTimeout(typeWriter, 500);
+        }
     }
-}
-function eraseText(){
-    if(textElements.innerHTML.length > 0){
-        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
-        setTimeout(eraseText, 50)
-    }
-    else{
-        textIndex = (textIndex + 1) % texts.length;
-        charcterIndex = 0;
-        setTimeout(typeWriter, 500)
-    }
-}
-window.onload = typeWriter;
+
+    // Start the typewriter effect on window load
+    window.onload = typeWriter;
+});
